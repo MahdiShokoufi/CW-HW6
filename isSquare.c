3256f1334404160e108342bb2ce941206966bac1
MahdiShokoufi <mahdishokoufi@gmail.com>
2022-12-03 08:10:30 +0330

    Revert "Make code faster"
    This reverts commit 0e444d00938f826af3bdfe2fa06bc15d13820cb1.

--- isSquare.c
+++ isSquare.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int isSquare(int x){
-  int i = 1;
-  while (i * i < x)
-    i ++;
-  return i * i == x;
+  for (int i = 1; i <= x; i ++)
+    if (i * i == x)
+      return 1;
+  return 0;
 }
 
 int main(){
