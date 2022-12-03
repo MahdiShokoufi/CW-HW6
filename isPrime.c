157bf230a08f83d0be637d0ae84fdde588f5a47b
MahdiShokoufi <46648836+MahdiShokoufi@users.noreply.github.com>
2022-12-03 07:37:13 +0330

    Handle corner cases

--- isPrime.c
+++ isPrime.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 int isPrime(int x){
+  if (x == 1)
+      return 0;
   for (int i = 2; i < x; i ++)
     if (x % i == 0)
       return 0;
- return 1;
+  return 1;
 }
 
 int main(){
